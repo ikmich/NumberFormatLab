50d66f88b3db343f8889e5eaf6ed99c651d4a1c2
ikmich <michfx@gmail.com>
2019-08-20 17:02:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/numberformatlab/LocaleCurrencyInput.java
@@ -0,0 +1,15 @@
+package com.example.numberformatlab;
+
+import android.widget.EditText;
+
+public class LocaleCurrencyInput {
+
+    EditText etInput;
+
+    public LocaleCurrencyInput() {
+    }
+
+    public void init(EditText etInput) {
+        this.etInput = etInput;
+    }
+}
